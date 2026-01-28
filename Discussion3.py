b3f650bf98ef0297d2032b74f1cbb7078a04beda
Andrew Rothstein <andrewrothstein@Andrews-MacBook-Pro-230.local>
2026-01-28 18:39:11 -0500

    <finished product>

--- Discussion3.py
+++ Discussion3.py
@@ -26,6 +26,9 @@ class Rectangle():
     # Returns a boolean value
 
     # YOUR CODE HERE
+    def __eq__(self, other):
+        return self.width == other.width and self.height == other.height
+    
 
 
     
